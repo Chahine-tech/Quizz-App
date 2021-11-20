cb1509a3b9e2b11a361f6590d819e98ba224056a
unknown <chahine.benlahcen@gmail.com>
2021-11-20 18:16:11 +0100

    setup backend

--- pages/quizz.js
+++ pages/quizz.js
@@ -4,13 +4,13 @@ import React, { useState } from 'react';
 
 export async function getStaticProps(context) {
     const res = await fetch(`http://localhost:3000/api/quizz`)
-    const {data} = await res.json()
+    const { data } = await res.json()
     return {
-      props: {
-          quizz: data.quizz
-      },
+        props: {
+            quizz: data.quizz
+        },
     }
-  }
+}
 export default function Quizz(props) {
     const [step, setStep] = useState(0)
     const [response, setReponse] = useState({})
@@ -29,10 +29,10 @@ export default function Quizz(props) {
                 <h1 className="text-center mb-6 text-xl font-bold">Quizz culture Général </h1>
 
                 <h2 className="text-xl font-bold p-8"> {props.quizz[0].questions[step].title}</h2>
-                <div className="space-x-2 p-8 mb-20"> 
+                <div className="space-x-2 p-8 mb-20">
                     <select class="select select-bordered w-full max-w-xs" onChange={(e) => setReponse(e.target.value)}>
                         <option disabled="disabled" selected="selected">Choisis ta réponse</option>
-                        {props.quizz[0].questions[step].responses.map(response => ( <option value={response.value}>{response.value}</option> ))}
+                        {props.quizz[0].questions[step].responses.map(response => (<option value={response.value}>{response.value}</option>))}
                     </select>
 
                     <button onClick={() => validateReponse(response)} class="btn btn-primary">Valider</button></div>
